3ac1bd3ceb3dfa18cc19f80c1876c75a39c251dd
KONTUR\ivansorokin <ivansorokin@skbkontur.ru>
2018-06-13 18:49:30 +0500

    - cleanup

--- TinyTimeline/Models/PresentationModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace TinyTimeline.Models
-{
-    public class PresentationModel
-    {
-        public ICollection<TimelineEventModel> Events { get; set; }
-    }
-}
