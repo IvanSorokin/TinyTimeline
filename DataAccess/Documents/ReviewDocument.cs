fab44b18fdd12acb22133ae3c61194ff21a8256c
KONTUR\ivansorokin <ivansorokin@skbkontur.ru>
2018-07-09 18:42:25 +0500

    - mongo attribute

--- DataAccess/Documents/ReviewDocument.cs
+++ DataAccess/Documents/ReviewDocument.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace DataAccess.Documents
 {
     public class ReviewDocument
     {
+        [BsonRepresentation(BsonType.String)]
         public Guid Id { get; set; }
         public string Content { get; set; }
         public int Rating { get; set; }
