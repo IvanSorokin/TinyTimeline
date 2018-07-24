cfd752ef1702c52193f4effe1283e1ae2908e415
KONTUR\ivansorokin <ivansorokin@skbkontur.ru>
2018-07-25 01:27:43 +0500

    - home controller

---
+++ TinyTimeline/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TinyTimeline.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => RedirectToAction("Sessions", "Presentation");
+    }
+}
