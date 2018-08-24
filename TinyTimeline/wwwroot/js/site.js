81d4fdc2a5d901bdd183a831edf89e958f1b2ee2
KONTUR\ivansorokin <ivansorokin@skbkontur.ru>
2018-08-24 17:17:57 +0500

    - merge scripts

--- TinyTimeline/wwwroot/js/site.js
+++ TinyTimeline/wwwroot/js/site.js
@@ -59,6 +59,25 @@ function deleteEvent(eventId, sessionId) {
     });
 }
 
+function selectEvent(eventId, sessionId) {
+    $('#' + eventId).addClass('picked_item');
+    let mergeButton = 'mergeButton' + eventId;
+    $('#buttons' + eventId).prepend(`<a class="btn btn-warning btn-xs" style="cursor: pointer" id="${mergeButton}">Merge</a>`);
+    $('#mergeButton' + eventId).click(() => mergeEvents(sessionId));
+    $('#select' + eventId).prop("onclick", null).off("click").text("Deselect").click(() => {deselectEvent(eventId, sessionId)});
+}
+
+function deselectEvent(eventId, sessionId) {
+    $('#' + eventId).removeClass('picked_item');
+    $('#select' + eventId).prop("onclick", null).off("click").text("Select").click(() => {selectEvent(eventId, sessionId)});
+    $('#mergeButton' + eventId).remove();
+}
+
+function mergeEvents(sessionId) {
+    let eventIds = $(".picked_item").toArray().map(x => x.id);
+    alert(eventIds);
+}
+
 function deleteReview(reviewId, sessionId) {
     $.ajax({
         type: 'DELETE',
